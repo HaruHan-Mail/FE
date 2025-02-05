6840b0d22759239a1c8d10dc7b40e70e8fe25b97
js4939 <kimjisu4939@gmail.com>
2025-02-05 17:16:52 +0900

    init: 프로젝트 초기 세팅

---
+++ src/components/Component2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Component2.css'
+
+const Component2 = () => {
+  return (
+    <div>Component2</div>
+  )
+}
+
+export default Component2
---
+++ src/components/Component3.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Component3.css'
+
+const Component3 = () => {
+  return (
+    <div>Component3</div>
+  )
+}
+
+export default Component3
