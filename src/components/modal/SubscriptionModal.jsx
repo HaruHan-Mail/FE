315c6ecea238882787eb6ac1b827b7a424a18fe0
Ryu Donghyeon <r12dh16@gmail.com>
2025-02-09 20:29:23 +0900

    feat: 모달 창 디자인 요소 추가

--- src/components/modal/SubscriptionModal.jsx
+++ src/components/modal/SubscriptionModal.jsx
@@ -7,38 +7,47 @@ const SubscriptionModal = ({ isOpen, onClose }) => {
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <span className="close-button" onClick={onClose}>&times;</span>
-        <h2><span style={{ color: "orange" }}>HaruHan</span>지식 구독</h2>
-        
-        {/* 수신 빈도 - 라디오 버튼으로 변경 */}
+        <span className="close-button" onClick={onClose}>
+          &times;
+        </span>
+        <h2>
+          <span style={{ color: "orange" }}>HaruHan</span>지식 구독
+        </h2>
+
+        {/* 수신 빈도 */}
         <p>수신 빈도</p>
         <label>
-          <input type="radio" name="frequency" /> 
+          <input type="radio" name="frequency" />
+          <span className="custom-checkbox" />
           하루 하나 (월~금)
         </label>
         <label>
-          <input type="radio" name="frequency" /> 
+          <input type="radio" name="frequency" />
+          <span className="custom-checkbox" />
           하루 다섯 (월요일)
         </label>
 
-        {/* 수신 시간 - 라디오 버튼으로 변경 */}
+        {/* 수신 시간 */}
         <p>수신 시간</p>
         <label>
-          <input type="radio" name="time" /> 
+          <input type="radio" name="time" />
+          <span className="custom-checkbox" />
           아침 8시
         </label>
         <label>
-          <input type="radio" name="time" /> 
+          <input type="radio" name="time" />
+          <span className="custom-checkbox" />
           오후 12시
         </label>
         <label>
-          <input type="radio" name="time" /> 
+          <input type="radio" name="time" />
+          <span className="custom-checkbox" />
           저녁 6시
         </label>
 
         {/* 이메일 입력 */}
         <p>이메일</p>
-        <input type="email" placeholder="example@gmail.com" />
+        <input type="email" placeholder="example@naver.com" />
 
         {/* 구독 버튼 */}
         <button className="subscribe-btn">구독하기</button>
