98b48cc2d14829c37b535f1cd1adf4742f110728
Jaekong_dev <qkrwoghd04@naver.com>
2025-03-05 22:31:59 +0900

    fix: vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: false,
-      pwaAssets: {
-        disabled: false,
-        preset: 'default',  // config: true 대신 preset: 'default' 사용
-      },
-      manifest: {
-        name: 'haruhan',
-        short_name: 'haruhan',
-        description: 'A Liberal Arts Subscription Service Project',
-        theme_color: '#ffffff',
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-      },
-      devOptions: {
-        enabled: false,
-        navigateFallback: 'index.html',
-        suppressWarnings: true,
-        type: 'module',
-      },
-    })
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://semtle.catholic.ac.kr:8082',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
-
+  plugins: [react()],
+});
