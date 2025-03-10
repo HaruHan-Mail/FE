aa83e1fd5e1047fdf6401878d7df413de63adf00
zzzzzisu <kimjisu4939@gmail.com>
2025-03-10 22:12:20 +0900

    feat: 메일 링크 페이지 헤더 생성

---
+++ src/components/Layout/MailHeader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../css/MailHeader.css';
+import LogoImg from '/src/assets/images/HaruhanLogo.png';
+
+const MailHeader = () => {
+  return (
+    <section className="MailHeaderContainer">
+      <a target="_blank" href="https://haruhan.site">
+        <img className="MailHeaderLogoImage" src={LogoImg} />
+      </a>
+    </section>
+  );
+};
+
+export default MailHeader;
