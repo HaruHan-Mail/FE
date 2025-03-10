a52b67599e243b431b362803bb08e680060ccb02
zzzzzisu <kimjisu4939@gmail.com>
2025-03-10 22:30:16 +0900

    fead: 피드백 페이지API 연동

---
+++ src/apis/feedbackApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://haruhan.site/api';
+
+export const feedbackSubmit = async (data) => {
+  const response = await axios.post(
+    `${BASE_URL}/feedback`,
+    { feedback_content: data },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+  return response.data;
+};
