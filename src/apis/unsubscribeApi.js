74aee31898cc9b43f60c751626bceb329d761ed3
Jaekong_dev <qkrwoghd04@naver.com>
2025-03-12 15:41:05 +0900

    fix: 비동기적으로 처리하고 있어서 구독 해지 확인이 안되던 문제 해결

--- src/apis/unsubscribeApi.js
+++ src/apis/unsubscribeApi.js
@@ -2,14 +2,25 @@ import axios from 'axios';
 
 const BASE_URL = 'https://haruhan.site/api';
 
-export const unsubscribeSubmit = async (email, token) => {
-  const response = await axios
-    .delete(`${BASE_URL}/user`, {
-      data: { email, token },
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then((res) => console.log(res.data));
-  return response.data;
+export const unsubscribeSubmit = async (data) => {
+  console.log('unsubscribeSubmit 호출');
+  try {
+    const response = await axios.delete(`${BASE_URL}/user`, {
+      data: data,
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (!response.data) {
+      return {
+        stateCode: response.status,
+        message: response.statusText || '요청이 성공적으로 처리되었습니다.',
+        data: 'Request processed successfully',
+      };
+    }
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw error.response.data;
+    }
+    throw error;
+  }
 };
