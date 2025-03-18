7e20e694bea0b329042d62298022634be90ddbeb
Jaekong_dev <qkrwoghd04@naver.com>
2025-03-18 21:08:30 +0900

    feat: tanstack을 통한 상태 관리 시도

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.css';
 import Home from './pages/Home.jsx';
 import NotFound from './pages/NotFound.jsx';
@@ -7,6 +9,8 @@ import Unsubscribe from './pages/Unsubscribe.jsx';
 import Feedback from './pages/Feedback.jsx';
 import Setting from './pages/Setting.jsx';
 import LoadingWrapper from './components/common/LoadingWrapper.jsx';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -37,8 +41,12 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
+  // return <RouterProvider router={router} />;
 }
 
 export default App;
