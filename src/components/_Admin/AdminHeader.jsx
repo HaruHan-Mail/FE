250b7bf2db0a7b1a270ba0b28eca040360b494df
qkrwoghd04 <qkrwoghd04@naver.com>
2025-07-03 11:57:56 +0900

    modify: 간단한 코드 변경

--- src/components/_Admin/AdminHeader.jsx
+++ src/components/_Admin/AdminHeader.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import './css/AdminSection.css';
 import './css/AdminHeader.css'
 
-const AdminHeader = ({ username }) => {
+const AdminHeader = () => {
   return (
     <header className="admin-header">
       <div className="admin-logo">
         <span>Admin Dashboard</span>
-      </div>
-      <div className="admin-user-info">
-        <span className="admin-user-name">{username}</span>
       </div>
     </header>
   );
