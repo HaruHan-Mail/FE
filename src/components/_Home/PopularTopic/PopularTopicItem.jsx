29d0043215178074843fe64aecfc288856e3001d
qkrwoghd04 <qkrwoghd04@naver.com>
2025-07-04 14:30:31 +0900

    refactor: PopularTopic swiper 적용

--- src/components/_Home/PopularTopic/PopularTopicItem.jsx
+++ src/components/_Home/PopularTopic/PopularTopicItem.jsx
@@ -4,44 +4,51 @@ import styled from '@emotion/styled';
 const Container = styled.div`
   width: 300px;
   height: 300px;
-  position: absolute;
-  box-sizing: border-box;
+  position: relative;
+  border-radius: 8px;
+  overflow: hidden;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
   font-size: 1.5rem;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 
   @media (min-width: 1024px) {
     width: 400px;
     height: 400px;
     font-size: 2rem;
   }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+    z-index: 1;
+  }
+`;
+
+const Content = styled.div`
+  color: white;
+  position: relative;
+  z-index: 2;
+  font-weight: 600;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
 
 const PopularTopicItem = ({ icon, title, image }) => {
-  const style = image
-    ? {
-        backgroundImage: `url(${image})`,
-        backgroundPosition: 'center',
-        filter: 'brightness(0.5)',
-        position: 'absolute',
-        zIndex: -1,
-        width: '100%',
-        height: '100%',
-        borderRadius: 10,
-      }
-    : {};
-
   return (
-    <>
-      <Container>
-        <img src={image} alt={title} style={style} />
-        <div>
-          {icon} {title}
-        </div>
-      </Container>
-    </>
+    <Container style={{ backgroundImage: `url(${image})` }}>
+      <Content>
+        {icon} {title}
+      </Content>
+    </Container>
   );
 };
 
--- src/components/_Home/PopularTopic/PopularTopicList.jsx
+++ src/components/_Home/PopularTopic/PopularTopicList.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Global, css } from '@emotion/react';
 import PopularTopicItem from './PopularTopicItem';
 import { usePopularContent } from '../../../hooks/queries';
 import { getPopularImages } from '../../../utils/getPopularImages';
 import LoadingSpinner from '../../common/LoadingSpinner';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 
 const Container = styled.div`
   width: 100%;
@@ -15,28 +23,103 @@ const Container = styled.div`
   position: relative;
 
   @media (min-width: 1024px) {
-    height: 450px;
+    height: 500px;
   }
 `;
 
+// Global Swiper 스타일
+const swiperStyles = css`
+  .swiper {
+    width: 100%;
+    padding-top: 50px;
+    padding-bottom: 50px;
+  }
+
+  .swiper-slide {
+    background-position: center;
+    background-size: cover;
+    width: 300px;
+    height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    @media (min-width: 1024px) {
+      width: 400px;
+      height: 400px;
+    }
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+  }
+  
+  
+`;
+
 const PopularTopicList = () => {
-  const { data: popularContent, isLoading } = usePopularContent();
+  const { data: popularContent, isLoading, error } = usePopularContent();
   const images = getPopularImages();
 
   if (isLoading) {
-    return <LoadingSpinner />;
+    return (
+      <Container>
+        <LoadingSpinner />
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container>
+        <div>인기 컨텐츠를 불러오는데 실패했습니다.</div>
+      </Container>
+    );
+  }
+
+  if (!popularContent || popularContent.length === 0) {
+    return (
+      <Container>
+        <div>인기 컨텐츠가 없습니다.</div>
+      </Container>
+    );
   }
 
   return (
-    <Container>
-      {popularContent.map((item, idx) => (
-        <PopularTopicItem 
-          key={item.id || idx} 
-          title={item.title} 
-          image={images[idx]} 
-        />
-      ))}
-    </Container>
+    <>
+      <Global styles={swiperStyles} />
+      <Container>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}
+          coverflowEffect={{
+            rotate: 45,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {popularContent.map((item, idx) => (
+            <SwiperSlide key={item.id || idx}>
+              <PopularTopicItem 
+                title={item.title} 
+                image={images[idx]} 
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Container>
+    </>
   );
 };
 
