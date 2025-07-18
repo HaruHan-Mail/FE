fba77ba5d0cf07b53bd278d3f4a800165fca12b0
qkrwoghd04 <qkrwoghd04@naver.com>
2025-07-18 11:39:06 +0900

    perf: 기존 popular 이미지 파일 확장자 jpg -> avif로 최적화

--- src/utils/getPopularImages.js
+++ src/utils/getPopularImages.js
@@ -1,8 +1,8 @@
-import PopularImg1 from '../assets/images/popular-1.jpg';
-import PopularImg2 from '../assets/images/popular-2.jpg';
-import PopularImg3 from '../assets/images/popular-3.jpg';
-import PopularImg4 from '../assets/images/popular-4.jpg';
-import PopularImg5 from '../assets/images/popular-5.jpg';
+import PopularImg1 from '../assets/images/popular-1.avif';
+import PopularImg2 from '../assets/images/popular-2.avif';
+import PopularImg3 from '../assets/images/popular-3.avif';
+import PopularImg4 from '../assets/images/popular-4.avif';
+import PopularImg5 from '../assets/images/popular-5.avif';
 
 export const getPopularImages = () => {
   return [PopularImg1, PopularImg2, PopularImg3, PopularImg4, PopularImg5];
