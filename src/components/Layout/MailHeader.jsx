5d8f3a50d00692456dce2f644f73d31200dd9977
qkrwoghd04 <qkrwoghd04@naver.com>
2025-07-19 23:17:31 +0900

    refactor: MailHeader clamp()를 활용 로고 배치 및 emotion 도입

--- src/components/Layout/MailHeader.jsx
+++ src/components/Layout/MailHeader.jsx
@@ -1,31 +1,36 @@
-import { useState, useEffect } from 'react';
 import './css/MailHeader.css';
 import LogoImg from '/src/assets/images/HaruhanLogo.webp';
+import styled from '@emotion/styled';
+
+const MailHeaderContainer = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+
+  padding: 20px 30px;
+
+`;
+
+const MailHeaderLogoImage = styled.img`
+  width: 100px;
+  height: 50px;
+`;
 
 const MailHeader = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) setIsScrolled(true);
-      else setIsScrolled(false);
-    };
-
-    // 스크롤할 때마다, handleScroll 함수 호출
-    window.addEventListener('scroll', handleScroll);
-
-    // 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
+ 
   return (
-    <section className="MailHeaderContainer" id={`${isScrolled ? 'Scrolled' : ''}`}>
+    <MailHeaderContainer>
       <a target="_blank" href="https://haruhan.site">
         <img className="MailHeaderLogoImage" src={LogoImg} fetchPriority='high' loading="eager" alt="Haruhan Logo" width={100} height={50} />
       </a>
-    </section>
+    </MailHeaderContainer>
   );
 };
 
